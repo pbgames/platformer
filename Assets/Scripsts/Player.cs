4b9843fad97924f6f2e6a5489d46852c64505e7b
AndriyPedan <andriypedan.p@gmail.com>
2017-09-21 18:05:10 +0300

    Implement left/right movement

---
+++ Assets/Scripsts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  private Rigidbody2D rigidbody;
+
+  [SerializeField]
+  private float movementSpeed;
+
+  private bool facingRight;
+
+  void Start() {
+    facingRight = true;
+    rigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate() {
+    float horizontal = Input.GetAxis("Horizontal");
+
+    HandleMovement(horizontal);
+    Flip(horizontal);
+  }
+
+  private void HandleMovement(float horizontal) {
+    rigidbody.velocity = new Vector2(horizontal * movementSpeed, rigidbody.velocity.y);
+  }
+
+  private void Flip(float horizontal) {
+    if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+      facingRight = !facingRight;
+
+      Vector3 scale = transform.localScale;
+      scale.x *= -1;
+      transform.localScale = scale;
+    }
+  }
+}
